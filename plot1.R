fc260db37980e5deb62e2e4f671cc325b76c88ab
butchie t <justin.markunas@gmail.com>
2015-08-04 18:06:46 -0400

    Project 1

---
+++ plot1.R
@@ -0,0 +1,14 @@
+#Boilerplate reading of file and subsetting for all plots of the project, ASSUMES DATA IS IN WORKING DIRECTORY
+
+consume <- read.table("household_power_consumption.txt",sep=";",header=TRUE,skip=66636,nrows=2880) 
+  #reads in data to consume from only dates of 2/1/2007 through 2/2/2007
+cols <- c("Date","Time","Global_active_power","Global_reactive_power","Voltage","Global_Intensity","Sub_metering_1","Sub_metering_2","Sub_metering_3")
+colnames(consume) <- cols   #rename header to match intial file
+consume$Date_and_Time <- as.POSIXlt(paste(consume$Date, consume$Time), format = "%d/%m/%Y %H:%M:%S")
+  #adds new column to data frame listing date and time as POSIXct class
+
+#Plotting and file generation commands:
+png(filename="plot1.png") #defaults 480x480 pixel image
+hist(consume$Global_active_power,xlab="Global Active Power (kilowatts)",main="Global Active Power", col ="red")
+dev.off()
+
---
+++ plot4.R
@@ -0,0 +1,30 @@
+#Boilerplate reading of file and subsetting for all plots of the project, ASSUMES DATA IS IN WORKING DIRECTORY
+
+consume <- read.table("household_power_consumption.txt",sep=";",header=TRUE,skip=66636,nrows=2880) 
+  #reads in data to consume from only dates of 2/1/2007 through 2/2/2007
+cols <- c("Date","Time","Global_active_power","Global_reactive_power","Voltage","Global_Intensity","Sub_metering_1","Sub_metering_2","Sub_metering_3")
+colnames(consume) <- cols   #rename header to match intial file
+consume$Date_and_Time <- as.POSIXlt(paste(consume$Date, consume$Time), format = "%d/%m/%Y %H:%M:%S")
+  #adds new column to data frame listing date and time as POSIXct class
+
+#Plotting and file generation commands:
+png(filename="plot4.png") #defaults 480x480 pixel image
+par(mfrow=c(2,2)) #2 by 2 panel of graphs
+
+#Plot 1:
+plot(consume$Date_and_Time, consume$Global_active_power, xlab="", ylab="Global Active Power", type="l")
+
+#Plot 2:
+plot(consume$Date_and_Time,consume$Voltage,xlab="datetime",ylab="Voltage",type="l")
+
+#Plot 3:
+plot(consume$Date_and_Time,consume$Sub_metering_1,xlab="",ylab="Energy sub metering",type="l")
+lines(consume$Date_and_Time,consume$Sub_metering_2,col="red")
+lines(consume$Date_and_Time,consume$Sub_metering_3,col="blue")
+legend("topright",col=c("black","red","blue"),legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),lwd=1,bty = "n")
+
+#Plot 4:
+plot(consume$Date_and_Time,consume$Global_reactive_power,ylab="Global_reactive_power",xlab="datetime",type="l")
+
+dev.off()
+
